447f1f2f4e0528d9310d4860bd526ca0c6f7ef39
Felix Fritzén <felixfritzen00@gmail.com>
2024-10-04 19:40:13 +0200

    [FEAT] class for training loop

--- training.py
+++ training.py
@@ -3,18 +3,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tqdm as tqdm
 import utils, model_functions
+import torch.nn as nn
 
 class FinetuneCLIP():
-  def __init__(self, dataloaders, clip, train_p, optimizer):
+  def __init__(self, dataloaders, clip):
     self.dataloaders = dataloaders
     self.loss = {'train':[], 'val':[]}
     self.es = {'pat': 10, 'curr_pat' :0, 'min_loss': np.inf}# early stop
     self.conf ={'epochs':200 }
     self.clip = clip # model and processor
-    self.train_p = train_p # trainable param
+    self.train_p = {} # Store trainable parameters here
     self.tt = {'soft':1, 'LoRA':0} # tuning method to use
-    self.optimizer = optimizer
-
+    self.optimizer = None # config in initialize
 
   def train(self):
     """Training loop"""
@@ -41,6 +41,7 @@ class FinetuneCLIP():
     if self.tt['soft']:
       text_embeds = model_functions.get_text_emb_soft(self.clip['m'], self.clip['p'], text, self.train_p['soft'])
       logits_per_image, loss = model_functions.apply_clip(text_embeds, image_embeds, self.clip['m'], train=train)
+    
     elif self.tt['LoRA']:
       pass
     else:
@@ -48,6 +49,24 @@ class FinetuneCLIP():
       logits_per_image, loss = model_functions.apply_clip(text_embeds, image_embeds, self.clip['m'], train=train)
 
     return logits_per_image, loss
+
+  def eval(self, show_image=False):
+    """Evaluate model on test set"""
+    all_predictions, all_labels = [], []
+    with torch.no_grad():
+      for batch_nr, (image_embeds, labels, images) in enumerate(tqdm(self.dataloaders['test'])):
+        logits_per_image, _ = self.forward(image_embeds, self.dataloaders['test'].dataset.classes)
+        # probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+        predicted_class = logits_per_image.argmax(dim=-1)
+        all_predictions.append(predicted_class)
+        for lab in labels:
+            all_labels.append(self.dataloaders['test'].dataset.class_to_id[lab])
+        if show_image and batch_nr%40==0:
+            images = utils.return_normal(images, self.clip['p'], 4, True)
+    all_predictions, all_labels=torch.cat(all_predictions).cpu(), torch.tensor(all_labels).cpu()
+    acc = utils.accuracy(all_predictions, all_labels)
+    print('Accuracy',acc)
+    return all_predictions, all_labels, acc
 
   def earlystop(self):
     """Stop training when val loss start to increase"""
@@ -74,24 +93,6 @@ class FinetuneCLIP():
                   self.es['min_loss'] = np.inf
                   self.es['curr_pat'] = 0
 
-  def eval(self, show_image=False):
-    """Evaluate model on test set"""
-    all_predictions, all_labels = [], []
-    with torch.no_grad():
-      for batch_nr, (image_embeds, labels, images) in enumerate(tqdm(self.dataloaders['test'])):
-        logits_per_image, _ = self.forward(image_embeds, self.dataloaders['test'].dataset.classes)
-        # probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-        predicted_class = logits_per_image.argmax(dim=-1)
-        all_predictions.append(predicted_class)
-        for lab in labels:
-            all_labels.append(self.dataloaders['test'].dataset.class_to_id[lab])
-        if show_image and batch_nr%40==0:
-            images = utils.return_normal(images, self.clip['p'], 4, True)
-    all_predictions, all_labels=torch.cat(all_predictions).cpu(), torch.tensor(all_labels).cpu()
-    acc = utils.accuracy(all_predictions, all_labels)
-    print('Accuracy',acc)
-    return all_predictions, all_labels, acc
-
   def plot_loss(self):
     plt.figure(figsize=(10, 6))
     plt.plot(list(range(1, len(self.loss['train'])+1)), self.loss['train'], label='Training Loss')
@@ -107,3 +108,16 @@ class FinetuneCLIP():
   def load_p(self):
     """Load trained parameters"""
     self.train_p['soft']=torch.load('soft_prompts.pth', weights_only=True)
+
+  def initialize(self, params):
+    """Initialize trainable parameters"""
+    self.train_p['add'] = params['add'] # the text added to classes
+
+    if self.tt['soft']:
+      self.train_p['soft'] = nn.Parameter(torch.zeros(params['num_soft'],
+                self.clip['m'].text_projection.in_features), requires_grad=True)
+      
+
+      self.optimizer = torch.optim.Adam([self.train_p['soft']], lr=1e-3)
+    if self.tt['LoRA']:
+      pass
