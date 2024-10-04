f8ae4665de433a95d75922b0808cb4737f72fc53
Felix Fritzén <felixfritzen00@gmail.com>
2024-10-04 19:52:44 +0200

    [FIX]

--- utils.py
+++ utils.py
@@ -1,109 +1,55 @@
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
+from torchvision.utils import make_grid
+from sklearn.metrics import classification_report
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 import tqdm as tqdm
-import utils, model_functions
 
-class FinetuneCLIP():
-  def __init__(self, dataloaders, clip, train_p, optimizer):
-    self.dataloaders = dataloaders
-    self.loss = {'train':[], 'val':[]}
-    self.es = {'pat': 10, 'curr_pat' :0, 'min_loss': np.inf}# early stop
-    self.conf ={'epochs':200 }
-    self.clip = clip # model and processor
-    self.train_p = train_p # trainable param
-    self.tt = {'soft':1, 'LoRA':0} # tuning method to use
-    self.optimizer = optimizer
-
-
-  def train(self):
-    """Training loop"""
-    self.clip['m'].train()
-    for epoch in tqdm(range(self.conf['epochs'])):
-      running_loss = 0.0
-      for batch_nr, (image_embeds, labels, images) in enumerate(self.dataloaders['train']):
-        self.optimizer.zero_grad()
-        logits_per_image, loss = self.forward(image_embeds, labels)
-        loss.backward()
-        self.optimizer.step()
-        running_loss +=loss.item()
-        #print(self.train_p['soft'].grad)
-      self.loss['train'].append(running_loss/len(self.dataloaders['train'])/len(labels))
-      if self.earlystop():
-        self.load_p()# get best found
-        return self.loss
-
-  def forward(self, image_embeds, labels):
-    """Get predictions of the model, add more here for different tuning methods"""
-    train = True if image_embeds.shape[0]==len(labels) else False
-    text = [self.train_p['add']+i for i in labels]
-    #image_embeds, _ = get_image_emb(model, processor, return_normal(images, processor, 0, False)) #SLOW
-    if self.tt['soft']:
-      text_embeds = model_functions.get_text_emb_soft(self.clip['m'], self.clip['p'], text, self.train_p['soft'])
-      logits_per_image, loss = model_functions.apply_clip(text_embeds, image_embeds, self.clip['m'], train=train)
-    elif self.tt['LoRA']:
-      pass
-    else:
-      text_embeds = model_functions.get_text_emb(self.clip['m'], self.clip['p'], text)
-      logits_per_image, loss = model_functions.apply_clip(text_embeds, image_embeds, self.clip['m'], train=train)
-
-    return logits_per_image, loss
-
-  def earlystop(self):
-    """Stop training when val loss start to increase"""
-    with torch.no_grad():
-      running_loss = 0.0
-      for batch_nr, (image_embeds, labels, images) in enumerate(self.dataloaders['val']):# val loader
-          _, loss = self.forward(image_embeds, labels)
-          running_loss +=loss.item()
-      self.loss['val'].append(running_loss/len(self.dataloaders['val'])/len(labels))
-      if len(self.loss['val'])>2:# early stop
-          if self.es['curr_pat'] ==0:
-              if running_loss> self.loss['val'][-2]: # if val_loss increase
-                  self.es['min_loss'] = running_loss
-                  torch.save(self.train_p['soft'], 'soft_prompts.pth')
-                  self.es['curr_pat']+=1
-          else:
-              if running_loss> self.es['min_loss']: # if val_loss continute to increase
-                  self.es['curr_pat']+=1
-                  curr , pat = self.es['curr_pat'], self.es['pat']
-                  print(f'Patience is {curr} / {pat}')
-                  if self.es['curr_pat'] >=self.es['pat']:
-                      return 'STOP'
-              else: #reset
-                  self.es['min_loss'] = np.inf
-                  self.es['curr_pat'] = 0
-
-  def eval(self, show_image=False):
-    """Evaluate model on test set"""
-    all_predictions, all_labels = [], []
-    with torch.no_grad():
-      for batch_nr, (image_embeds, labels, images) in enumerate(tqdm(self.dataloaders['test'])):
-        logits_per_image, _ = self.forward(image_embeds, self.dataloaders['test'].dataset.classes)
-        # probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-        predicted_class = logits_per_image.argmax(dim=-1)
-        all_predictions.append(predicted_class)
-        for lab in labels:
-            all_labels.append(self.dataloaders['test'].dataset.class_to_id[lab])
-        if show_image and batch_nr%40==0:
-            images = utils.return_normal(images, self.clip['p'], 4, True)
-    all_predictions, all_labels=torch.cat(all_predictions).cpu(), torch.tensor(all_labels).cpu()
-    acc = utils.accuracy(all_predictions, all_labels)
-    print('Accuracy',acc)
-    return all_predictions, all_labels, acc
-
-  def plot_loss(self):
-    plt.figure(figsize=(10, 6))
-    plt.plot(list(range(1, len(self.loss['train'])+1)), self.loss['train'], label='Training Loss')
-    plt.plot(list(range(1, len(self.loss['val'])+1)), self.loss['val'], label='Validation Loss')
-    # Adding labels and title
-    plt.title('Loss Over Datapoints')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.grid(True)
+def show_images(X):
+    """Given images as tensor shows them"""
+    np_images = X.numpy()
+    plt.figure(figsize=(10, 10))
+    grid_img = make_grid(torch.tensor(np_images))
+    plt.imshow(np.transpose(grid_img, (1, 2, 0)))
+    plt.axis('off')
     plt.show()
 
-  def load_p(self):
-    """Load trained parameters"""
-    self.train_p['soft']=torch.load('soft_prompts.pth', weights_only=True)
+def return_normal(tensor_image, processor, ant, plot):
+    """Reverse normalization and then show image, looks weird otherwise"""
+    mean = torch.tensor(processor.feature_extractor.image_mean).view(1, 3, 1, 1)# only colour channels
+    std = torch.tensor(processor.feature_extractor.image_std).view(1, 3, 1, 1)
+    tensor_image = tensor_image.cpu() * std + mean  # Add back the normalization
+    if plot:
+      show_images(tensor_image[0:ant,:,:,:])# do not show all
+    return tensor_image
+
+def accuracy(all_predictions, all_labels):
+    """ Given labels and predicted labels returns accuracy in %"""
+    correct= all_predictions==all_labels
+    return (100*correct.sum()/correct.shape[0]).item()
+
+def confussion_matrix(labels, pred_lab, categories, F1=True):
+  """ Given labels and predicted labels shows the confussion matrix"""
+  acc = accuracy(pred_lab, labels)
+  if F1:
+    print(classification_report(pred_lab, labels)) #F1
+  plt.figure(figsize=(8, 8))
+  cm = confusion_matrix(labels, pred_lab)
+  annot = np.where(cm != 0, cm, '')
+  sns.heatmap(cm, annot= annot, fmt="", cmap="Blues", cbar=True,xticklabels=categories, yticklabels=categories)
+  # Add labels
+  plt.xticks(rotation=45, ha="right", fontsize=10)
+  plt.title(f"Confusion Matrix, acc {acc:.2f} %", fontsize=15)
+  plt.xlabel("Predicted Labels", fontsize=12)
+  plt.ylabel("True Labels", fontsize=12)
+  # Show the plot
+  plt.show()
+
+def print_images(dataloader, processor, limit=10):
+  """Show a few images of the dataset to see errors"""
+  for batch_nr, (image_embeds, labels, images) in enumerate(tqdm(dataloader)):
+    if batch_nr<limit:
+      return_normal(images, processor, 4, True)
